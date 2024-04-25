493e45198d3e818a39c9d0246617a26e5be83cec
Jason Pien <jasonpien@jasonsmbp27380.devices.brown.edu>
2024-04-25 16:32:32 -0400

    rest

---
+++ code/model.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+from keras.layers import \
+       Conv2D, MaxPool2D, Dropout, Flatten, Dense, BatchNormalization
+
+import hyperparameters as hp
+from keras import losses
+from keras import optimizers
+from keras import regularizers
+
+class VGGModel(tf.keras.Model):
+    def __init__(self):
+        super(VGGModel, self).__init__()
+
+        self.optimizer = optimizers.Adam(hp.learning_rate)
+
+        self.vgg16 = [
+            # Block 1
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv1"),
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv2"),
+            MaxPool2D(2, name="block1_pool"),
+            # Block 2
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv1"),
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv2"),
+            MaxPool2D(2, name="block2_pool"),
+            # Block 3
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv1"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv2"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv3"),
+            MaxPool2D(2, name="block3_pool"),
+            # Block 4
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv3"),
+            MaxPool2D(2, name="block4_pool"),
+            # Block 5
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv3"),
+            MaxPool2D(2, name="block5_pool")
+        ]
+
+        for layer in self.vgg16:
+               layer.trainable = False
+
+
+        self.head = [
+            Flatten(),
+            Dense(64, activation=tf.keras.layers.LeakyReLU(alpha=0.01)),
+            Dense(64, activation=tf.keras.layers.LeakyReLU(alpha=0.01)),
+            BatchNormalization(),
+            Dropout(0.1),
+            Dense(2, activation='softmax') #2 classes = real or fake
+        ]
+        # self.head = [
+        #       Flatten(),
+        #       Dense(64, activation=tf.keras.layers.LeakyReLU(alpha=0.1)),
+        #       BatchNormalization(),
+        #       Dropout(0.1),
+        #       Dense(15, activation='softmax')
+        # ]
+
+        self.vgg16 = tf.keras.Sequential(self.vgg16, name="vgg_base")
+        self.head = tf.keras.Sequential(self.head, name="vgg_head")
+
+    def call(self, x):
+        """ Passes the image through the network. """
+
+        x = self.vgg16(x)
+        x = self.head(x)
+
+        return x
+
+    @staticmethod
+    def loss_fn(labels, predictions):
+        """ Loss function for model. """
+
+        return losses.SparseCategoricalCrossentropy()(labels, predictions)
